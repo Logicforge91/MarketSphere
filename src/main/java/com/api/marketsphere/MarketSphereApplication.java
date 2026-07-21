e788f37f715643a88ce8d68af0d7a9291552b36b
sumanks1307 <229777258+Logicforge91@users.noreply.github.com>
2026-07-21 19:15:01 +0530

    intial setup

---
+++ src/main/java/com/api/marketsphere/MarketSphereApplication.java
@@ -0,0 +1,13 @@
+package com.api.marketsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketSphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketSphereApplication.class, args);
+	}
+
+}
