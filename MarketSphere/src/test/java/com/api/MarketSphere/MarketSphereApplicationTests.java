10f17fa72f60a9a3ce447fff38a2e2cb6842ed3c
Kushal P <kushal20001115@gmail.com>
2026-07-21 20:51:36 +0530

    initial project

---
+++ MarketSphere/src/test/java/com/api/MarketSphere/MarketSphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.MarketSphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketSphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
